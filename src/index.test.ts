54eaae3953422b760054d9f15ed7d1c21ee52df0
CarlUdo <carl@wbsweden.com>
2024-09-04 19:27:29 +0200

    Update greeting test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,14 @@
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
-function greet() {}
+const greet = (name: string) => {
+  return {
+    message: `Welcome, ${name}!`
+  };
+};
 
-test("should greet person", () => {
-  const result = greet("Alek");
+test('should greet person', () => {
+  const result = greet('Carl');
 
-  deepEqual(result, { message: "Welcome, Alek!" });
+  deepEqual(result, { message: 'Welcome, Carl!' });
 });
