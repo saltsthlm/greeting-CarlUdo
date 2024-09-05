a79e5472fd269e2c1befcff00c3bb6f6b4b48b02
CarlUdo <carl@wbsweden.com>
2024-09-05 12:19:09 +0200

    Check for empty string

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,24 @@ import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
 const greet = (name: string) => {
+  let message = '';
+
+  if (name.length !== 0) {
+    message = `Welcome, ${name}!`;
+  } else {
+    message = `Welcome whoever you're. I'm sure you've a name. Please provide it.`;
+  }
+
   return {
-    message: `Welcome, ${name}!`
+    message: message
   };
 };
 
 test('should greet person', () => {
   const result = greet('Carl');
+  const secondResult = greet('');
 
   deepEqual(result, { message: 'Welcome, Carl!' });
+  deepEqual(secondResult, { message: `Welcome whoever you're. I'm sure you've a name. Please provide it.`} );
 });
+;
