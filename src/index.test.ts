af67204c3205ccb52a6f0f4888183c48cfa71e13
CarlUdo <carl@wbsweden.com>
2024-09-09 18:58:44 +0200

    Remove typo

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,6 @@ import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
 const greet = (name: string) => {
-  //let message = '';
   let message = `Welcome, ${name}!`; // One step less to do the initialisation directly
 
   if (name.length === 0) {
@@ -21,4 +20,3 @@ test('should greet person', () => {
   deepEqual(result, { message: 'Welcome, Carl!' });
   deepEqual(secondResult, { message: `Welcome whoever you're. I'm sure you've a name. Please provide it.`} );
 });
-;
